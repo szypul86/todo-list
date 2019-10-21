09650c4e7e97e2f3e78199504d77afc0bff7500d
Andrzej Szypulski <szypul86@gmail.com>
2019-10-21 23:59:13 +0200

    CRUD inner memory version done

---
+++ src/main/java/academy/learnprogramming/model/TodoData.java
@@ -0,0 +1,73 @@
+package academy.learnprogramming.model;
+
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.stream.Collectors;
+
+public class TodoData {
+
+    // fields
+    private static int idValue = 1;
+
+    private final List<TodoItem> items = new ArrayList<>();
+
+    public TodoData() {
+
+        addItem(new TodoItem("first", "first details", LocalDate.now()));
+        addItem(new TodoItem("second", "second details", LocalDate.now()));
+        addItem(new TodoItem("third", "third details", LocalDate.now()));
+    }
+
+    public List<TodoItem> getItems() {
+        return Collections.unmodifiableList(items);
+    }
+
+    public void addItem(@NonNull TodoItem toAdd) {
+        toAdd.setId(idValue);
+        items.add(toAdd);
+        idValue++;
+    }
+
+    public void removeItem(int id) {
+        ListIterator<TodoItem> itemListIterator = items.listIterator();
+
+        while (itemListIterator.hasNext()) {
+            TodoItem item = itemListIterator.next();
+            if (item.getId() == id) {
+                itemListIterator.remove();
+                break;
+            }
+        }
+    }
+
+    public TodoItem getItem(int id) {
+
+        //return items.stream().filter(item->item.getId()==id).collect(Collectors.toList()).get(0);
+
+        for (TodoItem item : items) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    public void updateItem(@NonNull TodoItem toUpdate) {
+        ListIterator<TodoItem> itemListIterator = items.listIterator();
+
+        while (itemListIterator.hasNext()) {
+            TodoItem item = itemListIterator.next();
+
+            if (item.equals(toUpdate)) {
+                itemListIterator.set(toUpdate);
+                break;
+            }
+        }
+    }
+
+}
--- src/main/java/academy/learnprogramming/model/TodoItem.java
+++ src/main/java/academy/learnprogramming/model/TodoItem.java
@@ -1,5 +1,29 @@
 package academy.learnprogramming.model;
 
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@EqualsAndHashCode(of = "id")
 public class TodoItem {
-    
+
+    // fields
+    //@Getter(AccessLevel.NONE)
+    //@Setter(AccessLevel.NONE)
+    private int id;
+    private String title;
+    private String details;
+    private LocalDate deadline;
+
+    //constructos
+    public TodoItem(String title, String details, LocalDate deadline) {
+        this.title = title;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+
+
+
 }
